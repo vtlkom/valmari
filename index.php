e786cffd567a4e0b73b480efcc77e74490646032
vitalik <vtlikom@gmai.com>
2023-05-10 19:55:46 +0300

    initial project

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+        include 'assets/templates/head.php';
+    ?>
+</head>
+
+<body>
+    <?php
+        include 'assets/templates/header.php';
+    ?>
+
+    <h1 class="title">hello</h1>
+
+    <?php
+        include 'assets/templates/footer.php';
+    ?>
+</body>
+
+</html>
